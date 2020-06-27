b261091e2b372f298c8aaee0478adb5c237a532d
crc <2268674269@qq.com>
2020-06-27 14:13:14 +0800

    1.7

--- 1/7.c
+++ 1/7.c
@@ -4,7 +4,8 @@
 
 double bisection(int p, int q, double (*func)(int, int, double));
 double f(int p, int q, double x);
-int main() {
+int main()
+{
     int p;
     int q;
     scanf("%d%d", &p, &q);
@@ -12,16 +13,22 @@ int main() {
     return 0;
 }
 
-double bisection(int p, int q, double (*func)(int, int, double)) {
-    int a,b,c;
-    a=-20;
-    b=20;
-    c=(a+b)/2;
-    while(f(p,q,c)>=EPSILON){
-        if(f(p,q,a)*f(p,q,c)<0){
+double bisection(int p, int q, double (*func)(int, int, double))
+{
+    double a;
+    double b;
+    double c;
+    a = -20;
+    b = 20;
+    c = (a + b) / 2;
+    while(fabs(func(p,q,c))>=EPSILON)
+    {
+        if(func(p,q,a)*func(p,q,c)<0)
+        {
             b=c;
         }
-        else{
+        else
+        {
             a=c;
         }
         c=(a+b)/2.0;
@@ -29,6 +36,7 @@ double bisection(int p, int q, double (*func)(int, int, double)) {
     return c;
 }
 
-double f(int p, int q, double x) {
+double f(int p, int q, double x)
+{
     return p * x + q;
 }
