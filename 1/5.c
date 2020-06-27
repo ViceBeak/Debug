50ff08ebe4d8966368ddb99c17823f4d037b39ba
crc <2268674269@qq.com>
2020-06-27 13:44:14 +0800

    1.5

--- 1/5.c
+++ 1/5.c
@@ -4,7 +4,8 @@
 
 double bisection(int p, int q, double (*func)(int, int, double));
 double f(int p, int q, double x);
-int main() {
+int main()
+{
     int p;
     int q;
     scanf("%d%d", &p, &q);
@@ -12,24 +13,32 @@ int main() {
     return 0;
 }
 
-double bisection(int p, int q, double (*func)(int, int, double)) {    double a;
+double bisection(int p, int q, double (*func)(int, int, double))
+{   
+    double a;
     double b;
     double c;
     a = 20;
     b = -20;
     c = (a + b)/ 2;
-    while(fabs(f(p,q,c)) >= EPSILON){
-        if((f(p,q,a) * f(p,q,c)) < 0) {
-            c = (c + a)/ 2, b = c,c = (a + b)/2;
+    while(fabs(f(p,q,c)) >= EPSILON)
+    {
+        if((func(p,q,a) * func(p,q,c)) > 0)
+        {
+            a = c;
+            c = (a + b)/ 2;
         }
-        if((f(p,q,b) * f(p,q,c)) < 0) {
-            c = (b + c)/ 2, a = c,c = (a + b)/2;
+        else if((func(p,q,b) * func(p,q,c)) > 0)
+        {
+            b = c;
+            c = (a + b)/ 2;
         }
     }
     return c;
         
 }
 
-double f(int p, int q, double x) {
+double f(int p, int q, double x)
+{
     return p * x + q;
 }
