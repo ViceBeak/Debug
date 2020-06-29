2a1ba0c9402cd376b1dd883bf0feec6cfa4a3321
crc <2268674269@qq.com>
2020-06-29 22:55:20 +0800

    3.7

--- 3/7.c
+++ 3/7.c
@@ -1,84 +1,72 @@
 #include <stdio.h>
- 
+
 int main()
 {
     int matrix[100][100];
     int m;
     int n;
-    int l = 1, i, j, k;
-    scanf("%d%d",&m,&n);
-    for (i = 0; i<m; i++) {
-          for (j = 0; j<n; j++) {
-             scanf("%d",&matrix[i][j]);
-         }
-     }
-     k = 0;
-     i = 0;
-   j = 0;
-    for (;;) {
+    int i;
+    int j;
+    int k;
+    int startX;
+    int startY;
+    int endX;
+    int endY;
 
-        for (; j<n - i; j++) {
-             printf("%d",matrix[i][j]);
-             k++;
-             if (k == m*n)
-                 break;
-             else
-                 printf(" ");
-             if (j == n - i - 1) {
-                 i++;
-                 l = 2;
-                break;
-             }
-         }
-         if (k == m*n)
-             break;
- 
-         for (; i<m - n + 1 + j; i++) {
-             printf("%d",matrix[i][j]) ;
-             k++;
-             if (k == m*n)
-                 break;
-             else 
-                 printf(" ");
-             if (i == m - n + j) {
-                 j--;
-                 l = 3;
-                 break;
-             }
-         }
-         if (k == m*n)
-             break;
-         for (; j >= m - 1 - i; j--) {
-             printf("%d",matrix[i][j]) ;
-             k++;
-             if (k == m*n)
-                 break;
-             else
-                 printf(" ");
-             if (j == m - i-1) {
-                 i--;
-                 l = 4;
-                 break;
-             }
-         }
-         if (k == m*n)
-             break;
-         for (; i>j; i--) {
-             printf("%d",matrix[i][j]) ;
-             k++;
-             if (k == m*n)
-                 break;
-             else
-                 printf(" ");
-             if (i == j + 1) {
-                 j++;
-                 l = 1;
-                 break;
-             }
-         }
- 
-         if (k == m*n)
-             break;
-     }
-     return 0;
- }
+    k = 0;
+    startX = 0;
+    startY = 0;
+    endX = m;
+    endY = n;
+   
+    scanf("%d %d", &m, &n);
+
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+   
+    while (startX * 2 < m && startY * 2 < n) {
+        endX = m - 1 - startX;
+        endY = n - 1 - startY;
+
+        if (startY < endY) {
+            for (j = startY; j <= endY; j++) {
+                printf("%d", matrix[startX][j]);
+                if (++k < m * n) {
+                    printf(" ");
+                }
+            }
+        }
+       
+        if (startX < endX) {
+            for (j = (startY == endY) ? startX : startX + 1; j <= endX; j++) {
+                printf("%d", matrix[j][endY]);
+                if (++k < m * n) {
+                    printf(" ");
+                }
+            }
+        }
+        if (endX > startX && startY < endY) {
+            for (j = endY - 1; j >= startY; j--) {
+                printf("%d", matrix[endX][j]);
+                if (++k < m * n) {
+                    printf(" ");
+                }
+            }
+        }
+        if (endX - 1 > startX && startY < endY) {
+            for (i = endX - 1; i >= startX + 1; i--) {
+                printf("%d", matrix[i][startX]);
+                if (++k < m * n) {
+                    printf(" ");
+                }
+            }
+        }
+        startX++;
+        startY++;
+    }
+    
+    return 0;
+} 
