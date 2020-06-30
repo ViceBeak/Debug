c95e88e87454e467413b57d8b7033bb0808d4745
ViceBeak <67231649+ViceBeak@users.noreply.github.com>
2020-06-30 15:47:16 +0800

    Update 3.c

--- 10/3.c
+++ 10/3.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() 
+int main()
 {
     int a;
     int b;
     int c;
-    
-    scanf("%d",&b);
+
+    scanf("%d", &b);
+
     if (b == 1) {
-    	return 0;
+        printf("");
     } else if (b == 2) {
-    	int x = 2;
-    	printf("%d",x);
+        printf("2");
     } else if (b > 2) {
-    	for (a = 3;a <= b;a = a + 1)  {
-        	int count = 0;
-            for (c = 2;c < a;c = c + 1) {
-				if (a % c == 0) {
-                    count = 1;
+        printf("2\n");
+
+        for (a = 3; a <= b; a += 2) {
+            for (c = 2; c <= a; c += 1) {
+                if (a % c == 0 && a != c) {
                     break;
-                } 
-        	}
-            if (count == 0){
-            	printf("%d\n",a);
-			}
-    	}
-	}
-	
+                } else if (a % c == 0 && a == c) {
+                    printf("%d\n", a);
+                }
+            }
+        }
+    }
+
     return 0;
 }
