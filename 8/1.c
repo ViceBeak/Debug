ec76650fcb46fc97e826686b860c6ec3b4033ebb
ViceBeak <67231649+ViceBeak@users.noreply.github.com>
2020-06-30 15:33:27 +0800

    Update 1.c

--- 8/1.c
+++ 8/1.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() 
+int main()
 {
-    int n;
-    
-    scanf("%d", &n);
-    getchar();
-    
-    char names[n][101];
-    char max[101];
-    
+    int  n;
+    char names[200][200];
+    char max[200];
+
+    scanf("%d\n", &n);
+
     for (int i = 0; i < n; i++) {
-         int z = 0;
-         while (scanf("%c", &names[i][z]) != EOF && names[i][z] != '\n') {
+        int z = 0;
+        while (scanf("%c", &names[i][z]) != EOF && names[i][z] != '\n') {
              z++;
-         }
+        }
     }
+
     strcpy(max, names[0]);
-    for (int i = 0; i < n - 1; i++) {
-        if (strlen(names[i]) > strlen(max)) {
+
+    for (int i = 0; i < n; i++) {
+        if (strlen(max) < strlen(names[i])) { 
             strcpy(max, names[i]);
         }
     }
-    printf("%s\n", max);
     
+    printf("%s", max);
+
     return 0;
 }
