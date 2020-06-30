ce5b49d651937e684e4dc2c5a5762b125c7bb64d
crc <2268674269@qq.com>
2020-06-30 11:18:12 +0800

    12.1-12.9

--- 12/5.c
+++ 12/5.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 #define PI 3.14159
-int main() {
-    float r,a,b;
+
+int main() 
+{
+    float r;
+	float a;
+	float b;
+    
     scanf("%f",&r);
-a=2*PI*r;
-    b=PI*r*r;
-    printf("%.2f\n",a);
- printf("%.2f",b);
+	a = 2 * PI * r;
+    b = PI * r * r;
+    printf("%.3f\n",a);
+    printf("%.3f",b);
+    
     return 0;
 }
--- 12/9.c
+++ 12/9.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 #define PI 3.14159
-int main() {
-	float r;
+
+int main() 
+{
+    float r;
     float c, s;
-	scanf("%g", &r);
+
+    scanf("%g", &r);
+	
     c = 2 * PI * r;
     s = PI * r *r;
-    printf("%.3f\n",c);
-    printf("%.3f\n",s);
+    
+    printf("%.3lf\n", c);
+    printf("%.3lf\n", s);
+    
     return 0;
 }
