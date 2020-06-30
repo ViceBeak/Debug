35203c82598a180adfe6ac80824171866b9ce19c
ViceBeak <67231649+ViceBeak@users.noreply.github.com>
2020-06-30 15:03:52 +0800

    Update 7.c

--- 5/7.c
+++ 5/7.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-void swap (char **b, char **c) {
-    char* temp=(char *)malloc(31);
-    temp = *b;
-    *b= *c;
-    *c = temp;
+
+void swap(char *s1,char *s2)
+{
+    char t[100];
+
+    strcpy(t, s1);
+    strcpy(s1, s2);
+    strcpy(s2, t);
 }
-int main() {
+
+int main() 
+{
     char name[20][31];
-    int i;
-    int j;
-    int m = 10;
+    int  i;
+    int  j;
+    int  m = 10;
+    
     for (i = 0; i < m; i++) {
         scanf("%s", name[i]);    
     }
+    
     for (j = 0; j < m; j++) {
-        for (i = 0; i <m-j-1; i ++) {
-            if(strcmp(name[i], name[i + 1]) > 0){ 
-                    swap(&name[i], &name[i + 1]);    
+        for (i = 0; i < m - j - 1; i ++) {
+            if (strcmp (name[i], name[i + 1]) > 0) { 
+                    swap(name[i], name[i + 1]);    
             }
         }
     }
+    
     for (i = 0; i < m; i++) {
         printf("%s\n", name[i]);    
     }
+    
     return 0;
 }
