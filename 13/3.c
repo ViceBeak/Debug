cdee5198aa108fb6796140cab7620daa2dcd3de7
ViceBeak <67231649+ViceBeak@users.noreply.github.com>
2020-06-30 16:10:28 +0800

    Update 3.c

--- 13/3.c
+++ 13/3.c
@@ -9,68 +9,68 @@ typedef struct node {
 Node *circle_create(int n);
 void count_off(Node *head, int n, int k, int m);
 
-int main() 
+int main()
 {
     int n, k, m;
-    scanf("%d %d %d", &n, &k, &m);
+    scanf("%d%d%d", &n, &k, &m);
     Node *head = circle_create(n);
     count_off(head, n, k, m);
-    
     return 0;
 }
 
-Node *circle_create(int n) 
+Node *circle_create(int n)
 {
     Node *temp, *new_node, *head;
     int i;
 
-    temp = (Node *) malloc(sizeof(Node));
+    temp = (Node *)malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
-    for(i = 2; i <= n; i++) {
-        new_node = (Node *) malloc(sizeof(Node));
+    for (i = 2; i <= n; i++) {
+        new_node = (Node *)malloc(sizeof(Node));
         new_node->data = i;
         temp->next = new_node;
         temp = new_node;
     }
+
     temp->next = head;
 
     return head;
 }
 
-void count_off(Node *head, int n, int k, int m) 
+void count_off(Node *head, int n, int k, int m)
 {
-    int  x;
-	int  y;
-    Node *find;
-    Node *temp;
-    Node *pre;
-    
+    Node *temp, *prev;
     temp = head;
-    for (y = 0;y < n;y++) {
-        if (y == 0) {
-			for (x = 1;x < k;x++) {
-            	find = temp;
-                temp = temp->next;
-        	}
-        	for (x = 1;x < k - 1;x++) {
-            	pre = head;
-                head = head->next;
-        	}
-		}
-		for (x = 0;x < m;x++) {
-			find = temp;
+    prev = temp;
+    
+    if (k == 1) {
+        for (int i = 0; i < n; i++) {
+            prev = temp;
             temp = temp->next;
-            pre = pre->next;
-		}
-		printf("%d",find->data);
-		pre->next = find->next;
-		find = find->next;
-    	if (y != n) {
-			printf(" ");
-		}
-	}
+        }
+    } else {
+        while (temp->data != k) {
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+
+    int count = 0;
+
+    while (count < n) {
+    	for (int i = 1; i < m; i++) {
+    		prev = temp;
+    		temp = temp->next;
+    	}
+    	printf("%d ", temp->data);
+    	count++;
+    	Node *p = temp;
+    	prev->next = p->next;
+    	temp = p->next;
+    	free(p);
+    }
 
     return;
 }
